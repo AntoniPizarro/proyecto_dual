dddc92fdb0c95cee75302cdfb0bfc76a649fa141
AntoniPizarro <apizarro@cifpfbmoll.eu>
2020-11-10 00:10:37 +0100

    generador de paginas WEB

---
+++ Generador_paginas_WEB.py
@@ -0,0 +1,15 @@
+import os
+archivo = "Mi nave"
+direccion = r"C:\Users\Granpepinillo\Desktop\\"
+file = open(direccion + archivo + ".html", "w")
+file.write("<!DOCTYPE html>" + os.linesep)
+file.write("<html lang=\"en\">" + os.linesep)
+file.write("<head>" + os.linesep)
+file.write("    <meta charset=\"UTF-8\">" + os.linesep)
+file.write("    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">" + os.linesep)
+file.write("    <title>Document</title>" + os.linesep)
+file.write("</head>" + os.linesep)
+file.write("<body>" + os.linesep)
+file.write("</body>" + os.linesep)
+file.write("</html>" + os.linesep)
+file.close()

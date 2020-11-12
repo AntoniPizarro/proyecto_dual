04bc35269ea6c6bcc6c4d752392ee35be7be2af6
pau <pllinas@cifpfbmoll.eu>
2020-11-12 19:51:09 +0100

    cambio de prueba

--- python/Scraping.py
+++ python/Scraping.py
@@ -1,7 +1,8 @@
 from io import open
-a = open("a.html","r")
-b=open("b.txt","w")
-text=a.read()
-b.write(text[text.find("ind") : text.find("ind") + 10])
+a = open("a.html", "r")
+b = open("b.txt", "w")
+text = a.read()
+b.write(text[text.find("ind"): text.find("ind") + 10])
+b.write(text[text.find("h3")+3: text.find("/h3") - 1])
 a.close()
 b.close()

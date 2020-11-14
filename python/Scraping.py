0f6ec048008281755dbde072df268b0fcc701e6a
AntoniPizarro <apizarro@cifpfbmoll.eu>
2020-11-14 13:52:23 +0100

    Sincro

--- python/Scraping.py
+++ python/Scraping.py
@@ -1,7 +1,9 @@
-from io import open
+import os
 a = open("a.html","r")
 b=open("b.txt","w")
 text=a.read()
-b.write(text[text.find("ind") : text.find("ind") + 10])
+b.write(text[text.find("ind") : text.find("ind") + 10] + os.linesep)
+b.write(text[text.find("cat") : text.find("cat") + 13] + os.linesep)
+b.write(text[text.find("Gam") : text.find("Gam") + 9] + os.linesep)
 a.close()
 b.close()

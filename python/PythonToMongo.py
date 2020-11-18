9d8794c181c9691532d7e461c26c1ca6725aff0a
AntoniPizarro <apizarro@cifpfbmoll.eu>
2020-11-18 13:32:44 +0100

    feat: Se han añadido archivos Python que hacen la funcion de Scrapping Program y conectar a MongoDB
    Se han implementado dos archivos que sirven como base para desarrollar un programa que pueda hacer WEB Scrapping y otro que permite conectarse a MongoDB y gestionar las bases de datos

---
+++ python/PythonToMongo.py
@@ -0,0 +1,4 @@
+from pymongo import MongoClient
+cliente = MongoClient('mongodb+srv://m001-student:m001-mongodb-basics@sandbox.4uubd.mongodb.net/accion?retryWrites=true&w=majority')
+db = cliente['high_schools']
+print(db.list_collection_names())

d7d9f75820bf9a07d8b1bb5729566065b03a4126
pau <pllinas@cifpfbmoll.eu>
2020-11-18 22:18:52 +0100

    feat: :zap: Adding "https" to the links
    improve the performance of the web crawler

--- python/Scraping.py
+++ python/Scraping.py
@@ -2,7 +2,10 @@
 
 import requests
 
+
 def obtenerCodigo(url):
+    if url.find('https') == -1:
+        url = "https://paulk123.000webhostapp.com/" + url
     r = requests.get(url)
     codigoWeb = r.text
     print(codigoWeb)

180fabd93c099449ef4d97b3ead920506af92dd5
AntoniPizarro <apizarro@cifpfbmoll.eu>
2020-11-22 21:58:56 +0100

    feat: Sincronizacion de ramas
    Se ha copiado el codigo de la rama principal para que coincida con la rama de trabajo

--- python/Scraping.py
+++ python/Scraping.py
@@ -3,21 +3,24 @@
 import requests
 
 
-def obtenerCodigo(url):
-    if len(url) < 1 or type(url) != str:
+def obtenerCodigo(webCrawler):
+    if len(webCrawler) < 1 or type(webCrawler) != str:
         return False
-    cabecera = "https://paulk123.000webhostapp.com/"
-    if url.find('https') == -1:
-        url = cabecera + url
-    r = requests.get(url)
+    cabecera = "https://proyectodual.000webhostapp.com/"
+    if webCrawler.find('https') == -1:
+        webCrawler = cabecera + webCrawler
+    r = requests.get(webCrawler)
     codigoWeb = r.text
     return codigoWeb
-    
-def crawler(url):
-    codigo = obtenerCodigo(url)
+
+
+def getLinks(url):
+    codigo = url
+    if url == -1:
+        codigo = obtenerCodigo(url)
     listaLinks = []
     while True:
-        inicio_href = codigo.find("<a href=")
+        inicio_href = codigo.find("href=")
         inicio_url = codigo.find('"', inicio_href)
         fin_url = codigo.find('"', inicio_url + 1)
         if len(codigo) == 0:
@@ -25,13 +28,34 @@ def crawler(url):
         elif inicio_href == -1:
             break
         else:
-            listaLinks.append(codigo[inicio_url + 1: fin_url])
-            codigo = codigo[fin_url:]
+            link = codigo[inicio_url + 1: fin_url]
+            if link.find("html") == -1:
+                pass
+                codigo = codigo[fin_url:]
+            else:
+                listaLinks.append(codigo[inicio_url + 1: fin_url])
+                codigo = codigo[fin_url:]
     return listaLinks
 
 
+def union(p, q):
+    for e in q:
+        if e not in p:
+            p.append(e)
+
+
+def webCrawler(seed):
+    toCrawl = [seed]
+    crawled = []
+    while toCrawl:
+        page = toCrawl.pop()
+        if page not in crawled:
+            union(toCrawl, getLinks(obtenerCodigo(page)))
+            crawled.append(page)
+    return crawled
+
+
 if __name__ == "__main__":
-    assert crawler("https://paulk123.000webhostapp.com/") == ["Free_Lily.html","movie_rank.html","videos.html","audios.html"]
-
-
-
+    assert webCrawler("https://proyectodual.000webhostapp.com/") == ['https://proyectodual.000webhostapp.com/',
+                                                                     './contacto.html', './catalogo.html', 'baja.html', 'media.html', 'alta.html', './index.html']
+    #assert webCrawler("https://paulk123.000webhostapp.com/") == ['https://paulk123.000webhostapp.com/', 'audios.html', 'videos.html', 'movie_rank.html', 'Free_Lily.html']

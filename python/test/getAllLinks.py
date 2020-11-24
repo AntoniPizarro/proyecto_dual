a72bbb2a3fb910297b00c13f560d02465e53f918
pau <pllinas@cifpfbmoll.eu>
2020-11-24 03:23:56 +0100

    test: :white_check_mark: Casos test get all links
    creamos los 6 casos test para la función get all links

--- python/test/getAllLinks.py
+++ python/test/getAllLinks.py
@@ -4,11 +4,31 @@ import pytest
 # pytest -v test/getAllLinks.py
 
 
-def test_getIndexLinks():
+def test_IndexLinks():
     assert getLinks(obtenerCodigo("https://proyectodual.000webhostapp.com/")
                     ) == ["./index.html", "./catalogo.html", "./contacto.html"]
 
 
-def test_getCatalogLinks():
-    assert getLinks(obtenerCodigo("https://proyectodual.000webhostapp.com/./catalogo.html")) == ['https://proyectodual.000webhostapp.com/', './contacto.html', './catalogo.html', 'transports/v-wing.html', '../contacto.html', '../catalogo.html', '../index.html', 'transports/imperial-shuttle.html', 'transports/gr-75.html',
-                                                                                                 'transports/crucero-alderaan.html', 'transports/aa-9.html', 'transports/twilight.html', 'transports/cañonera-republica.html', 'transports/neimoidian-escort.html', 'transports/magna-guard.html', 'transports/t70-xwing.html', 'transports/y-wing.html', 'baja.html', 'media.html', 'alta.html', './index.html']
+def test_contactosLinks():
+    assert getLinks(obtenerCodigo("https://proyectodual.000webhostapp.com/contacto.html")
+                    ) == ['./index.html', './catalogo.html', './contacto.html']
+
+
+def test_linksOtraPaginaWebIndex():
+    assert getLinks(obtenerCodigo("https://paulk123.000webhostapp.com/")) == [
+        'Free_Lily.html', 'movie_rank.html', 'videos.html', 'audios.html', 'test-html.html']
+
+
+def test_linksOtraPaginaWebMovieRank():
+    assert getLinks(obtenerCodigo("https://paulk123.000webhostapp.com/movie_rank.html")
+                    ) == ['Free_Lily.html', 'movie_rank.html', 'videos.html', 'audios.html']
+
+
+def test_linksNaveCatalogo():
+    assert getLinks(obtenerCodigo(
+        "https://proyectodual.000webhostapp.com/transports/y-wing.html")) == ['../index.html', '../catalogo.html', '../contacto.html']
+
+
+def test_CatalogoLinks():
+    assert getLinks(obtenerCodigo("https://proyectodual.000webhostapp.com/catalogo.html")) == ['./index.html', './catalogo.html', './contacto.html', 'alta.html', 'media.html', 'baja.html', 'transports/y-wing.html',
+                                                                                               'transports/t70-xwing.html', 'transports/magna-guard.html', 'transports/neimoidian-escort.html', 'transports/cañonera-republica.html', 'transports/twilight.html', 'transports/aa-9.html', 'transports/crucero-alderaan.html', 'transports/gr-75.html', 'transports/imperial-shuttle.html', 'transports/v-wing.html']
